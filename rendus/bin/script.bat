e93df541400765aacf2fff9d5f4968989b726ceb
Arugakente <arugakente@gmail.com>
2021-01-20 21:29:35 +0100

    Assignment 4 final test

--- rendus/bin/script.bat
+++ rendus/bin/script.bat
@@ -1,6 +1,9 @@
 @echo off
-RayTracer.exe scene01-camera-ss-reflect-lights-shadows.yaml
+RayTracer.exe scene01-camera-2ss-reflect-lights-shadows.yaml
+RayTracer.exe scene01-camera-4ss-reflect-lights-shadows.yaml
+RayTracer.exe scene01-camera-2ss-reflect-lights-shadows-800x800.yaml
 RayTracer.exe scene01-zoom-ss-reflect-lights-shadows.yaml
+RayTracer.exe scene01-zoom-ss-reflect-lights-shadows-newCam.yaml
 RayTracer.exe scene01-xstretch-zoom-ss-reflect-lights-shadows.yaml
 RayTracer.exe scene01-velocity-zoom-ss-reflect-lights-shadows.yaml
 RayTracer.exe scene01-alpha-ss-reflect-lights-shadows.yaml
--- rendus/src/camera.h
+++ rendus/src/camera.h
@@ -22,7 +22,7 @@ public:
 	Camera(Triple e, Triple c, Triple u,double xs, int w, int h, double eT, int as,int aSample) : eye(e), center(c), up(u), xStretch(xs), viewWidth(w), viewHeight(h),exposureTime(eT), apertureSize(as),apertureSample(aSample)
 	{
 		Vector front = (eye - center).normalized();
-		side = up.cross(front).normalized();
+		side = up.cross(front);
 	}
 
 	//fonction to return the Ray pointing at a pixel
