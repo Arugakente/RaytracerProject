42a8cb6a780d873a6b7f932fd99f244d18f208f2
Arugakente <arugakente@gmail.com>
2021-01-30 15:00:08 +0100

    glm File (semi raw)

---
+++ raytracerframework_cpp/mesh.cpp
@@ -0,0 +1,19 @@
+#include "mesh.h"
+#include <iostream>
+#include <math.h>
+
+Hit Mesh::intersect(const Ray &ray)
+{
+
+}
+
+Vector Mesh::getUV(Point hit, Vector n)
+{
+
+}
+
+
+Point Mesh::getHit(double u, double v)
+{
+
+}
---
+++ raytracerframework_cpp/mesh.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "object.h"
+#include "glm.h"
+
+class Mesh : public Object
+{
+	GLMmodel* model;
+
+
+	Mesh(Point position, Triple rotation, Triple velocity, char* filename) : Object(position, rotation, velocity) 
+	{
+		model = glmReadOBJ(filename);
+	}
+
+	virtual Hit intersect(const Ray &ray);
+	Vector getUV(Point hit, Vector n);
+	Point getHit(double u, double v);
+};
+
