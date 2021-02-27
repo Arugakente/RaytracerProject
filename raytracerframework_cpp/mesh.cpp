714e76192201ee0ec8eaaae0f08c58fe76c9e353
Arugakente <arugakente@gmail.com>
2021-02-27 14:30:52 +0100

    Texcoords working (+ final scenes start)

--- raytracerframework_cpp/mesh.cpp
+++ raytracerframework_cpp/mesh.cpp
@@ -28,42 +28,43 @@ Hit Mesh::intersect(const Ray &ray)
 	}
 	if (!hasIntersected) return Hit::NO_HIT();
 
-	std::cout << r << std::endl << std::endl;
+	Ray transformedRay = transform(ray);
 	return res;
 }
 
 std::pair<Triangle, GLMtriangle*>* Mesh::getTriangle(Point hit)
 {
 	int i = 0;
-	for (auto t_pair : triangles) {
-
-		//hit = applyTransformation(hit);
+	for (auto& t_pair : triangles) {
 
 		Triangle t = t_pair.first;
 		Vector N = (t.v2Offset - t.v1Offset).cross(t.v3Offset - t.v1Offset);
 		N.normalize();
 
-		std::cout << i << " : " << abs((hit - t.position).dot(N)) << std::endl;
+		Ray r = Ray(hit, Vector(0, 0, 0)); //we use a Ray just to transform the hit (to be in a triangle's reference)
+		r = transform(r);
+		r = t.transform(r);
 
-		
-		//if (abs((hit - t.position).dot(N)) > 0.1) continue;
+		Point newHit = r.O;
 
-		//std::cout << "hello" << std::endl;
 
-		Vector e1 = t.v2Offset - t.v1Offset;
-		Vector e2 = t.v3Offset - t.v2Offset;
-		Vector e3 = t.v1Offset - t.v3Offset;
+		if (abs(newHit.dot(N)) < 0.000001) {
 
-		Vector C0 = hit - t.v1Offset;
-		Vector C1 = hit - t.v2Offset;
-		Vector C2 = hit - t.v3Offset;
+			Vector e1 = t.v2Offset - t.v1Offset;
+			Vector e2 = t.v3Offset - t.v2Offset;
+			Vector e3 = t.v1Offset - t.v3Offset;
 
-		if (N.dot(e1.cross(C0)) > 0 &&
-			N.dot(e2.cross(C1)) > 0 &&
-			N.dot(e3.cross(C2)) > 0) {
-			std::cout << i << std::endl;
-			return &t_pair;
+			Vector C0 = newHit - t.v1Offset;
+			Vector C1 = newHit - t.v2Offset;
+			Vector C2 = newHit - t.v3Offset;
+
+			if (N.dot(e1.cross(C0)) > 0 &&
+				N.dot(e2.cross(C1)) > 0 &&
+				N.dot(e3.cross(C2)) > 0) {
+				return &t_pair;
+			}
 		}
+		
 		i++;
 	}
 	return nullptr;
@@ -72,26 +73,35 @@ std::pair<Triangle, GLMtriangle*>* Mesh::getTriangle(Point hit)
 
 Vector Mesh::getUV(Point hit, Vector n)
 {
-	std::cout << hit << std::endl;
-
 	if (model->numtexcoords == 0) return Vector(0, 0, 0); //has no texture coordinates
 
 	std::pair<Triangle, GLMtriangle*>* t_pair = getTriangle(hit); //we get the corresponding triangle
 	if (t_pair == nullptr) std::cerr << "This point is not in a triangle" << std::endl;
 
+
 	Triangle t = t_pair->first;
 	GLMtriangle* tr = t_pair->second;
 
-	//texcoords Interpolation, taken from codeplea.com/triangular-interpolation
+	Ray r = Ray(hit, Vector(0, 0, 0)); //we use a Ray just to transform the hit (to be in a triangle's reference)
+	r = transform(r);
+	r = t.transform(r);
 
-	double w1 = (t.v2Offset.y - t.v3Offset.y) * (hit.x - t.v3Offset.x) + (t.v3Offset.x - t.v2Offset.x) * (hit.y - t.v3Offset.y) /
-		( (t.v2Offset.y - t.v3Offset.y) * (t.v1Offset.x - t.v3Offset.x) + (t.v3Offset.x - t.v2Offset.x) * (t.v1Offset.y - t.v3Offset.y) );
-	double w2 = (t.v3Offset.y - t.v1Offset.y) * (hit.x - t.v3Offset.x) + (t.v1Offset.x - t.v3Offset.x) * (hit.y - t.v3Offset.y) /
-		((t.v2Offset.y - t.v3Offset.y) * (t.v1Offset.x - t.v3Offset.x) + (t.v3Offset.x - t.v2Offset.x) * (t.v1Offset.y - t.v3Offset.y));
-	double w3 = 1 - w1 - w2;
+	Point newHit = r.O;
 
-	double u = w1 * (model->texcoords + 3 * tr->tindices[0])[0] + w2 * (model->texcoords + 3 * tr->tindices[1])[0] + w3 * (model->texcoords + 3 * tr->tindices[2])[0];
-	double v = w1 * (model->texcoords + 3 * tr->tindices[0])[1] + w2 * (model->texcoords + 3 * tr->tindices[1])[1] + w3 * (model->texcoords + 3 * tr->tindices[2])[1];
+
+	//texcoords Interpolation
+	double delta = ((t.v1Offset - t.v2Offset).cross(t.v1Offset - t.v3Offset)).length();
+
+	double w1 = ((newHit -t.v2Offset).cross(newHit -t.v3Offset)).length() / delta;
+	double w2 = ((newHit -t.v1Offset).cross(newHit -t.v3Offset)).length() / delta;
+	double w3 = ((newHit -t.v1Offset).cross(newHit -t.v2Offset)).length() / delta;
+
+	double u = w1 * (model->texcoords + 2 * tr->tindices[0])[0] + 
+		       w2 * (model->texcoords + 2 * tr->tindices[1])[0] + 
+		       w3 * (model->texcoords + 2 * tr->tindices[2])[0];
+	double v = w1 * (model->texcoords + 2 * tr->tindices[0])[1] +
+		       w2 * (model->texcoords + 2 * tr->tindices[1])[1] +
+		       w3 * (model->texcoords + 2 * tr->tindices[2])[1];
 
 	return Vector(u, v);
 }
--- raytracerframework_cpp/scene.cpp
+++ raytracerframework_cpp/scene.cpp
@@ -372,7 +372,7 @@ void Scene::render(Image &img)
 			double r=c*sqrt(n);
 			double th=n*goldenAngle;
 
-			//#pragma omp parallel for
+			#pragma omp parallel for
     		for (int y = 0; y < h; y++)
 			{
         		for (int x = 0; x < w; x++)
